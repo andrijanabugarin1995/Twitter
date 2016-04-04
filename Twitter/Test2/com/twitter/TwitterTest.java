47218fe623ce839893e69d318b8f8e87d4b88bd9
andrijanabugarin1995 <andrijana.bugarin@gmail.com>
2016-04-04 23:54:00 +0200

    Uradjen test i za drugu klasu, Twitter

---
+++ Twitter/Test2/com/twitter/TwitterTest.java
@@ -0,0 +1,49 @@
+package com.twitter;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.twitter.poruke.TwitterPoruka;
+
+public class TwitterTest {
+	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testVratiSvePoruke() {
+	   
+	}
+
+	@Test
+	public void testUnesi() {
+		
+	}
+
+	@Test
+	public void testVratiPoruke() {
+		
+	}
+
+}
